d4749988d06ed38abeef73338e752071b20786d5
aa.mezencev <aa.mezencev@tensor.ru>
2018-06-22 11:23:32 +0300

    specified behaviour view(noot finished)

--- app/src/main/java/com/example/aamezencev/handbook/common/view/AbstractActivity.kt
+++ app/src/main/java/com/example/aamezencev/handbook/common/view/AbstractActivity.kt
@@ -1,5 +1,8 @@
 package com.example.aamezencev.handbook.common.view
 
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import com.example.aamezencev.handbook.common.presenter.MvpPresenter
 import com.example.aamezencev.handbook.common.viewModel.MvpViewModel
@@ -7,4 +10,42 @@ import com.example.aamezencev.handbook.common.viewModel.MvpViewModel
 abstract class AbstractActivity<VM : MvpViewModel, Presenter : MvpPresenter<VM>>
     : AppCompatActivity(), AndroidComponent {
 
+    override val activityComponent: Activity
+        get() = this
+    override val fragmentManagerComponent: FragmentManager
+        get() = supportFragmentManager
+
+    var presenter: Presenter? = null
+        protected set
+    var viewModel: VM? = null
+        protected set
+
+    abstract fun injectDi()
+    abstract fun createPresenter(): Presenter
+    abstract fun createViewModel(): VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        injectDi()
+        presenter = createPresenter()
+        viewModel = createViewModel()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (viewModel != null)
+            presenter?.attachView(viewModel!!, this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter?.detachView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter?.destroy()
+        presenter = null
+        viewModel = null
+    }
 }
--- app/src/main/java/com/example/aamezencev/handbook/common/view/AbstractFragment.kt
+++ app/src/main/java/com/example/aamezencev/handbook/common/view/AbstractFragment.kt
@@ -1,10 +1,50 @@
 package com.example.aamezencev.handbook.common.view
 
+import android.app.Activity
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import com.example.aamezencev.handbook.common.presenter.MvpPresenter
 import com.example.aamezencev.handbook.common.viewModel.MvpViewModel
 
 abstract class AbstractFragment<VM : MvpViewModel, Presenter : MvpPresenter<VM>>
     : Fragment(), AndroidComponent {
 
+    override val activityComponent: Activity
+        get() = activity as Activity
+    override val fragmentManagerComponent: FragmentManager
+        get() = activity?.supportFragmentManager as FragmentManager
+
+    abstract fun injectDi()
+    abstract fun createPresenter(): Presenter
+    abstract fun createViewModel(): VM
+
+    var viewModel: VM? = null
+        protected set
+    var presenter: Presenter? = null
+        protected set
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        injectDi()
+        presenter = createPresenter()
+        viewModel = createViewModel()
+    }
+
+    override fun onAttachFragment(childFragment: Fragment?) {
+        super.onAttachFragment(childFragment)
+        if (viewModel != null) presenter?.attachView(viewModel!!, this)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        presenter?.detachView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        presenter?.destroy()
+        presenter = null
+        viewModel = null
+    }
 }
