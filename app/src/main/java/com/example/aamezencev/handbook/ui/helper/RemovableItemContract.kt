d0f195a67aa9627e2f840a5fe06d8e9ba2c2251c
aa.mezencev <aa.mezencev@tensor.ru>
2018-08-20 12:26:28 +0300

    updated RemovableItem

--- app/src/main/java/com/example/aamezencev/handbook/ui/helper/RemovableItemContract.kt
+++ app/src/main/java/com/example/aamezencev/handbook/ui/helper/RemovableItemContract.kt
@@ -1,6 +1,5 @@
 package com.example.aamezencev.handbook.ui.helper
 
-import android.support.v7.widget.RecyclerView
 import com.example.aamezencev.handbook.ui.view.RemovableItem
 
 interface RemovableItemContract {
--- app/src/main/java/com/example/aamezencev/handbook/ui/view/RemovableItem.kt
+++ app/src/main/java/com/example/aamezencev/handbook/ui/view/RemovableItem.kt
@@ -1,7 +1,6 @@
 package com.example.aamezencev.handbook.ui.view
 
 import android.content.Context
-import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -10,11 +9,6 @@ import android.widget.FrameLayout
 import com.example.aamezencev.handbook.R
 
 class RemovableItem : FrameLayout {
-    @LayoutRes
-    private var foregroundViewRes: Int? = null
-    @LayoutRes
-    private var backgroundViewRes: Int? = null
-
     var foregroundView: View? = null
         private set
     var backgroundView: View? = null
@@ -23,20 +17,46 @@ class RemovableItem : FrameLayout {
     constructor(context: Context) : super(context)
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        var background = -1
+        var foreground = -1
+
         context.obtainStyledAttributes(attrs, R.styleable.RemovableItem).apply {
-            backgroundViewRes = getResourceId(R.styleable.RemovableItem_rmv_background, -1)
-            foregroundViewRes = getResourceId(R.styleable.RemovableItem_rmv_foreground, -1)
+            background = getResourceId(R.styleable.RemovableItem_rmv_background, -1)
+            foreground = getResourceId(R.styleable.RemovableItem_rmv_foreground, -1)
             recycle()
         }
 
-        backgroundView = initLayer(backgroundViewRes)
-        foregroundView = initLayer(foregroundViewRes)
+        addLayers(background, foreground)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    constructor(context: Context, @LayoutRes background: Int, @LayoutRes foreground: Int) : super(context) {
+        addLayers(background, foreground)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val foregroundHeight = foregroundView?.measuredHeight
+        val backgroundHeight = backgroundView?.measuredHeight
+        if (foregroundHeight != null && backgroundHeight != null)
+            setMeasuredDimension(measuredWidth, Math.min(foregroundHeight, backgroundHeight))
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        foregroundView?.apply {
+            backgroundView?.layout(this.left, this.top, this.right, this.bottom)
+        }
+    }
+
+    private fun addLayers(background: Int, foreground: Int) {
+        backgroundView = initLayer(background)
+        foregroundView = initLayer(foreground)
 
         this.addView(backgroundView)
         this.addView(foregroundView)
     }
-
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private fun initLayer(value: Int?): View? {
         return value?.takeIf { it != -1 }?.run {
